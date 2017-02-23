aa4d48e2f9b1e1e00684d0db5ca3c484f49c5d68
emre.kirmizi <emre.kirmizi@unitbilisim.com>
2017-02-23 14:02:39 +0300

    new line indentation.

--- Source/eu.modelwriter.core.alloyinecore.ui/src/eu/modelwriter/core/alloyinecore/ui/editor/autoedit/AIEAutoEditStrategy.java
+++ Source/eu.modelwriter.core.alloyinecore.ui/src/eu/modelwriter/core/alloyinecore/ui/editor/autoedit/AIEAutoEditStrategy.java
@@ -4,20 +4,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.DefaultIndentLineAutoEditStrategy;
 import org.eclipse.jface.text.DocumentCommand;
-import org.eclipse.jface.text.IAutoEditStrategy;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 
-public class AIEAutoEditStrategy implements IAutoEditStrategy {
+public class AIEAutoEditStrategy extends DefaultIndentLineAutoEditStrategy {
 
-  private void configureCaret(final DocumentCommand command, int caretInc) {
+  private void configureCaret(final DocumentCommand command, final int caretInc) {
     command.caretOffset = command.offset + caretInc;
     command.shiftsCaret = false;
   }
 
   @Override
   public void customizeDocumentCommand(final IDocument document, final DocumentCommand command) {
+    super.customizeDocumentCommand(document, command);
     if (command.text.equals("(")) {
       command.text = "()";
       configureCaret(command, 1);
@@ -31,23 +32,23 @@ public class AIEAutoEditStrategy implements IAutoEditStrategy {
       command.text = "\"\"";
       configureCaret(command, 1);
     } else if (command.text.equals("{")) {
-      String indent = getIndent(document, command);
+      final String indent = getIndent(document, command);
       command.text = "\n" + indent + "{\n" + indent + "    \n" + indent + "}";
       configureCaret(command, 3 + indent.length() * 2 + 4);
     }
   }
 
-  private String getIndent(IDocument document, DocumentCommand command) {
+  private String getIndent(final IDocument document, final DocumentCommand command) {
     String result = "";
     try {
-      IRegion info = document.getLineInformationOfOffset(command.offset);
-      String text = document.get(info.getOffset(), info.getLength());
-      Matcher matcher = Pattern.compile("^(\\s)*(\\w)").matcher(text);
+      final IRegion info = document.getLineInformationOfOffset(command.offset);
+      final String text = document.get(info.getOffset(), info.getLength());
+      final Matcher matcher = Pattern.compile("^(\\s)*(\\w)").matcher(text);
       if (matcher.find()) {
-        int indexOf = text.indexOf(matcher.group(2));
+        final int indexOf = text.indexOf(matcher.group(2));
         result = text.substring(0, indexOf);
       }
-    } catch (BadLocationException e) {
+    } catch (final BadLocationException e) {
       result = "";
     }
     return result;
