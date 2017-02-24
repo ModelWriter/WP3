268b5dd24341633d52c9a3b6cb542013c4a0687e
Yunus Emre Şeker <y3seker@gmail.com>
2017-02-24 18:39:02 +0300

    EcoreTranslator now preserves element order, closes #48

--- Source/eu.modelwriter.core.alloyinecore.mapping/src/eu/modelwriter/core/alloyinecore/ui/mapping/AnnotationSources.java
+++ Source/eu.modelwriter.core.alloyinecore.mapping/src/eu/modelwriter/core/alloyinecore/ui/mapping/AnnotationSources.java
@@ -10,112 +10,109 @@ import org.eclipse.emf.ecore.ETypedElement;
 
 public interface AnnotationSources {
 
-  public final static String BASE = "http://www.modelwriter.eu/alloyinecore";
-  public final static String IMPORT = AnnotationSources.BASE + "/import";
-  public static final String MODULE = AnnotationSources.BASE + "/module";
-  public static final String OPTIONS = AnnotationSources.BASE + "/options";
-  public static final String SOURCE = AnnotationSources.BASE + "/source";
+  String BASE = "http://www.modelwriter.eu/alloyinecore";
+  String IMPORT = AnnotationSources.BASE + "/import";
+  String MODULE = AnnotationSources.BASE + "/model";
+  String OPTIONS = AnnotationSources.BASE + "/options";
+  String SOURCE = AnnotationSources.BASE + "/source";
 
-  public final static String VISIBILITY = AnnotationSources.BASE + "/visibility";
-  public final static String STATIC = AnnotationSources.BASE + "/static";
-  public final static String QUALIFIER = AnnotationSources.BASE + "/qualifier";
-  public final static String NULLABLE = AnnotationSources.BASE + "/nullable";
-  public final static String MODEL = AnnotationSources.BASE + "/model";
-  public final static String GHOST = AnnotationSources.BASE + "/ghost";
+  String VISIBILITY = AnnotationSources.BASE + "/visibility";
+  String STATIC = AnnotationSources.BASE + "/static";
+  String QUALIFIER = AnnotationSources.BASE + "/qualifier";
+  String NULLABLE = AnnotationSources.BASE + "/nullable";
+  String MODEL = AnnotationSources.BASE + "/model";
+  String GHOST = AnnotationSources.BASE + "/ghost";
 
-  public final static String ATTR_EXPRESSIONS = AnnotationSources.BASE + "/attribute/expressions";
-  public final static String REF_EXPRESSIONS = AnnotationSources.BASE + "/reference/expressions";
+  String ATTR_EXPRESSIONS = AnnotationSources.BASE + "/attribute/expressions";
+  String REF_EXPRESSIONS = AnnotationSources.BASE + "/reference/expressions";
 
-  public final static String INVARIANT = AnnotationSources.BASE + "/expression/invariant";
-  public static final String INITIAL = AnnotationSources.BASE + "/expression/initial";
-  public static final String DERIVATION = AnnotationSources.BASE + "/expression/derivation";
+  String INVARIANT = AnnotationSources.BASE + "/expression/invariant";
+  String INITIAL = AnnotationSources.BASE + "/expression/initial";
+  String DERIVATION = AnnotationSources.BASE + "/expression/derivation";
 
-  public final static String PRECONDITION = AnnotationSources.BASE + "/operation/precondition";
-  public final static String POSTCONDITION = AnnotationSources.BASE + "/operation/postcondition";
-  public final static String BODY = AnnotationSources.BASE + "/operation/body";
+  String PRECONDITION = AnnotationSources.BASE + "/operation/precondition";
+  String POSTCONDITION = AnnotationSources.BASE + "/operation/postcondition";
+  String BODY = AnnotationSources.BASE + "/operation/body";
 
-  public final static String DATATYPE_PRIMITIVE = AnnotationSources.BASE + "/datatype/primitive";
+  String DATATYPE_PRIMITIVE = AnnotationSources.BASE + "/datatype/primitive";
 
-  public static boolean isStatic(final EModelElement element) {
+  static boolean isStatic(final EModelElement element) {
     return element.getEAnnotation(AnnotationSources.STATIC) != null;
   }
 
-  public static boolean isPrimitive(final EDataType dataType) {
+  static boolean isPrimitive(final EDataType dataType) {
     return dataType.getEAnnotation(AnnotationSources.DATATYPE_PRIMITIVE) != null;
   }
 
-  public static boolean isNullable(final ETypedElement element) {
+  static boolean isNullable(final ETypedElement element) {
     boolean canBe = element.getUpperBound() == -1 || element.getUpperBound() > 1;
     return canBe && element.getEAnnotation(AnnotationSources.NULLABLE) != null;
   }
 
+  public static boolean isAIEAnnotation(final EAnnotation eAnnotation) {
+    return eAnnotation.getSource() != null && eAnnotation.getSource().startsWith(BASE);
+  }
+
   /**
-   *
    * @param element @EModelElement to get annotations from
    * @return Filtered annotations of AlloyInEcore
    */
-  public static List<EAnnotation> filterAnnotations(final EModelElement element,
-      final String filter) {
+  static List<EAnnotation> filterAnnotations(final EModelElement element, final String filter) {
     return element.getEAnnotations().stream().filter(anno -> filter.equals(anno.getSource()))
         .collect(Collectors.toList());
   }
 
   /**
-   *
    * @param element @EModelElement to get annotations from
    * @return Invariant annotations of AlloyInEcore
    */
-  public static List<EAnnotation> getInvariants(final EModelElement element) {
+  static List<EAnnotation> getInvariants(final EModelElement element) {
     return AnnotationSources.filterAnnotations(element, AnnotationSources.INVARIANT);
   }
 
   /**
-   *
    * @param element @EModelElement to get annotations from
    * @return Derivation annotations of AlloyInEcore
    */
-  public static List<EAnnotation> getDerivation(final EModelElement element) {
+  static List<EAnnotation> getDerivation(final EModelElement element) {
     return AnnotationSources.filterAnnotations(element, AnnotationSources.DERIVATION);
   }
 
   /**
-   *
    * @param element @EModelElement to get annotations from
    * @return Initial annotations of AlloyInEcore
    */
-  public static List<EAnnotation> getInitial(final EModelElement element) {
+  static List<EAnnotation> getInitial(final EModelElement element) {
     return AnnotationSources.filterAnnotations(element, AnnotationSources.INITIAL);
   }
 
   /**
-   *
    * @param element @EModelElement to get annotations from
    * @return The Import annotations
    */
-  public static List<EAnnotation> getImports(final EModelElement element) {
+  static List<EAnnotation> getImports(final EModelElement element) {
     return AnnotationSources.filterAnnotations(element, AnnotationSources.IMPORT);
   }
 
   /**
-   *
    * @param element @EModelElement to get annotations from
    * @return The annotations other than AlloyInEcore
    */
-  public static List<EAnnotation> getAnnotations(final EModelElement element) {
+  static List<EAnnotation> getAnnotations(final EModelElement element) {
     return element.getEAnnotations().stream().filter(
         anno -> anno.getSource() == null || !anno.getSource().startsWith(AnnotationSources.BASE))
         .collect(Collectors.toList());
   }
 
-  public static List<EAnnotation> getPostconditions(final EModelElement element) {
+  static List<EAnnotation> getPostconditions(final EModelElement element) {
     return AnnotationSources.filterAnnotations(element, AnnotationSources.POSTCONDITION);
   }
 
-  public static List<EAnnotation> getPreconditions(final EModelElement element) {
+  static List<EAnnotation> getPreconditions(final EModelElement element) {
     return AnnotationSources.filterAnnotations(element, AnnotationSources.PRECONDITION);
   }
 
-  public static List<EAnnotation> getBodyExpressions(final EModelElement element) {
+  static List<EAnnotation> getBodyExpressions(final EModelElement element) {
     return AnnotationSources.filterAnnotations(element, AnnotationSources.BODY);
   }
 }
--- Source/eu.modelwriter.core.alloyinecore/src/main/java/eu/modelwriter/core/alloyinecore/internal/AnnotationSources.java
+++ Source/eu.modelwriter.core.alloyinecore/src/main/java/eu/modelwriter/core/alloyinecore/internal/AnnotationSources.java
@@ -34,112 +34,110 @@ import org.eclipse.emf.ecore.ETypedElement;
 
 public interface AnnotationSources {
 
-  String BASE = "http://www.modelwriter.eu/alloyinecore";
-  String IMPORT = AnnotationSources.BASE + "/import";
-  String MODULE = AnnotationSources.BASE + "/model";
-  String OPTIONS = AnnotationSources.BASE + "/options";
-  String SOURCE = AnnotationSources.BASE + "/source";
+    String BASE = "http://www.modelwriter.eu/alloyinecore";
+    String IMPORT = AnnotationSources.BASE + "/import";
+    String MODULE = AnnotationSources.BASE + "/model";
+    String OPTIONS = AnnotationSources.BASE + "/options";
+    String SOURCE = AnnotationSources.BASE + "/source";
 
-  String VISIBILITY = AnnotationSources.BASE + "/visibility";
-  String STATIC = AnnotationSources.BASE + "/static";
-  String QUALIFIER = AnnotationSources.BASE + "/qualifier";
-  String NULLABLE = AnnotationSources.BASE + "/nullable";
-  String MODEL = AnnotationSources.BASE + "/model";
-  String GHOST = AnnotationSources.BASE + "/ghost";
+    String VISIBILITY = AnnotationSources.BASE + "/visibility";
+    String STATIC = AnnotationSources.BASE + "/static";
+    String QUALIFIER = AnnotationSources.BASE + "/qualifier";
+    String NULLABLE = AnnotationSources.BASE + "/nullable";
+    String MODEL = AnnotationSources.BASE + "/model";
+    String GHOST = AnnotationSources.BASE + "/ghost";
 
-  String ATTR_EXPRESSIONS = AnnotationSources.BASE + "/attribute/expressions";
-  String REF_EXPRESSIONS = AnnotationSources.BASE + "/reference/expressions";
+    String ATTR_EXPRESSIONS = AnnotationSources.BASE + "/attribute/expressions";
+    String REF_EXPRESSIONS = AnnotationSources.BASE + "/reference/expressions";
 
-  String INVARIANT = AnnotationSources.BASE + "/expression/invariant";
-  String INITIAL = AnnotationSources.BASE + "/expression/initial";
-  String DERIVATION = AnnotationSources.BASE + "/expression/derivation";
+    String INVARIANT = AnnotationSources.BASE + "/expression/invariant";
+    String INITIAL = AnnotationSources.BASE + "/expression/initial";
+    String DERIVATION = AnnotationSources.BASE + "/expression/derivation";
 
-  String PRECONDITION = AnnotationSources.BASE + "/operation/precondition";
-  String POSTCONDITION = AnnotationSources.BASE + "/operation/postcondition";
-  String BODY = AnnotationSources.BASE + "/operation/body";
+    String PRECONDITION = AnnotationSources.BASE + "/operation/precondition";
+    String POSTCONDITION = AnnotationSources.BASE + "/operation/postcondition";
+    String BODY = AnnotationSources.BASE + "/operation/body";
 
-  String DATATYPE_PRIMITIVE = AnnotationSources.BASE + "/datatype/primitive";
+    String DATATYPE_PRIMITIVE = AnnotationSources.BASE + "/datatype/primitive";
 
-  static boolean isStatic(final EModelElement element) {
-    return element.getEAnnotation(AnnotationSources.STATIC) != null;
-  }
+    static boolean isStatic(final EModelElement element) {
+        return element.getEAnnotation(AnnotationSources.STATIC) != null;
+    }
 
-  static boolean isPrimitive(final EDataType dataType) {
-    return dataType.getEAnnotation(AnnotationSources.DATATYPE_PRIMITIVE) != null;
-  }
+    static boolean isPrimitive(final EDataType dataType) {
+        return dataType.getEAnnotation(AnnotationSources.DATATYPE_PRIMITIVE) != null;
+    }
 
-  static boolean isNullable(final ETypedElement element) {
-    boolean canBe = element.getUpperBound() == -1 || element.getUpperBound() > 1;
-    return canBe && element.getEAnnotation(AnnotationSources.NULLABLE) != null;
-  }
+    static boolean isNullable(final ETypedElement element) {
+        boolean canBe = element.getUpperBound() == -1 || element.getUpperBound() > 1;
+        return canBe && element.getEAnnotation(AnnotationSources.NULLABLE) != null;
+    }
 
-  /**
-   *
-   * @param element @EModelElement to get annotations from
-   * @return Filtered annotations of AlloyInEcore
-   */
-  static List<EAnnotation> filterAnnotations(final EModelElement element,
-                                                    final String filter) {
-    return element.getEAnnotations().stream().filter(anno -> filter.equals(anno.getSource()))
-        .collect(Collectors.toList());
-  }
+    static boolean isAIEAnnotation(final EAnnotation eAnnotation) {
+        return eAnnotation.getSource() != null && eAnnotation.getSource().startsWith(BASE);
+    }
 
-  /**
-   *
-   * @param element @EModelElement to get annotations from
-   * @return Invariant annotations of AlloyInEcore
-   */
-  static List<EAnnotation> getInvariants(final EModelElement element) {
-    return AnnotationSources.filterAnnotations(element, AnnotationSources.INVARIANT);
-  }
+    /**
+     * @param element @EModelElement to get annotations from
+     * @return Filtered annotations of AlloyInEcore
+     */
+    static List<EAnnotation> filterAnnotations(final EModelElement element,
+                                               final String filter) {
+        return element.getEAnnotations().stream().filter(anno -> filter.equals(anno.getSource()))
+                .collect(Collectors.toList());
+    }
 
-  /**
-   *
-   * @param element @EModelElement to get annotations from
-   * @return Derivation annotations of AlloyInEcore
-   */
-  static List<EAnnotation> getDerivation(final EModelElement element) {
-    return AnnotationSources.filterAnnotations(element, AnnotationSources.DERIVATION);
-  }
+    /**
+     * @param element @EModelElement to get annotations from
+     * @return Invariant annotations of AlloyInEcore
+     */
+    static List<EAnnotation> getInvariants(final EModelElement element) {
+        return AnnotationSources.filterAnnotations(element, AnnotationSources.INVARIANT);
+    }
 
-  /**
-   *
-   * @param element @EModelElement to get annotations from
-   * @return Initial annotations of AlloyInEcore
-   */
-  static List<EAnnotation> getInitial(final EModelElement element) {
-    return AnnotationSources.filterAnnotations(element, AnnotationSources.INITIAL);
-  }
+    /**
+     * @param element @EModelElement to get annotations from
+     * @return Derivation annotations of AlloyInEcore
+     */
+    static List<EAnnotation> getDerivation(final EModelElement element) {
+        return AnnotationSources.filterAnnotations(element, AnnotationSources.DERIVATION);
+    }
 
-  /**
-   *
-   * @param element @EModelElement to get annotations from
-   * @return The Import annotations
-   */
-  static List<EAnnotation> getImports(final EModelElement element) {
-    return AnnotationSources.filterAnnotations(element, AnnotationSources.IMPORT);
-  }
+    /**
+     * @param element @EModelElement to get annotations from
+     * @return Initial annotations of AlloyInEcore
+     */
+    static List<EAnnotation> getInitial(final EModelElement element) {
+        return AnnotationSources.filterAnnotations(element, AnnotationSources.INITIAL);
+    }
 
-  /**
-   *
-   * @param element @EModelElement to get annotations from
-   * @return The annotations other than AlloyInEcore
-   */
-  static List<EAnnotation> getAnnotations(final EModelElement element) {
-    return element.getEAnnotations().stream().filter(
-        anno -> anno.getSource() == null || !anno.getSource().startsWith(AnnotationSources.BASE))
-        .collect(Collectors.toList());
-  }
+    /**
+     * @param element @EModelElement to get annotations from
+     * @return The Import annotations
+     */
+    static List<EAnnotation> getImports(final EModelElement element) {
+        return AnnotationSources.filterAnnotations(element, AnnotationSources.IMPORT);
+    }
 
-  static List<EAnnotation> getPostconditions(final EModelElement element) {
-    return AnnotationSources.filterAnnotations(element, AnnotationSources.POSTCONDITION);
-  }
+    /**
+     * @param element @EModelElement to get annotations from
+     * @return The annotations other than AlloyInEcore
+     */
+    static List<EAnnotation> getAnnotations(final EModelElement element) {
+        return element.getEAnnotations().stream().filter(
+                anno -> anno.getSource() == null || !anno.getSource().startsWith(AnnotationSources.BASE))
+                .collect(Collectors.toList());
+    }
 
-  static List<EAnnotation> getPreconditions(final EModelElement element) {
-    return AnnotationSources.filterAnnotations(element, AnnotationSources.PRECONDITION);
-  }
+    static List<EAnnotation> getPostconditions(final EModelElement element) {
+        return AnnotationSources.filterAnnotations(element, AnnotationSources.POSTCONDITION);
+    }
 
-  static List<EAnnotation> getBodyExpressions(final EModelElement element) {
-    return AnnotationSources.filterAnnotations(element, AnnotationSources.BODY);
-  }
+    static List<EAnnotation> getPreconditions(final EModelElement element) {
+        return AnnotationSources.filterAnnotations(element, AnnotationSources.PRECONDITION);
+    }
+
+    static List<EAnnotation> getBodyExpressions(final EModelElement element) {
+        return AnnotationSources.filterAnnotations(element, AnnotationSources.BODY);
+    }
 }
