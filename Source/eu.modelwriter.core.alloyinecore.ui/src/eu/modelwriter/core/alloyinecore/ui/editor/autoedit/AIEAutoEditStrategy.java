5a5566611002e57d51427942ee630f49581e478f
emre.kirmizi <emre.kirmizi@unitbilisim.com>
2017-03-01 16:59:09 +0300

    auto edit, curly parenthesis behavior changed.

--- Source/eu.modelwriter.core.alloyinecore.ui/src/eu/modelwriter/core/alloyinecore/ui/editor/autoedit/AIEAutoEditStrategy.java
+++ Source/eu.modelwriter.core.alloyinecore.ui/src/eu/modelwriter/core/alloyinecore/ui/editor/autoedit/AIEAutoEditStrategy.java
@@ -1,13 +1,8 @@
 package eu.modelwriter.core.alloyinecore.ui.editor.autoedit;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.DefaultIndentLineAutoEditStrategy;
 import org.eclipse.jface.text.DocumentCommand;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
 
 public class AIEAutoEditStrategy extends DefaultIndentLineAutoEditStrategy {
 
@@ -32,25 +27,9 @@ public class AIEAutoEditStrategy extends DefaultIndentLineAutoEditStrategy {
       command.text = "\"\"";
       configureCaret(command, 1);
     } else if (command.text.equals("{")) {
-      final String indent = getIndent(document, command);
-      command.text = "\n" + indent + "{\n" + indent + "    \n" + indent + "}";
-      configureCaret(command, 3 + indent.length() * 2 + 4);
+      command.text = "{}";
+      configureCaret(command, 1);
     }
   }
 
-  private String getIndent(final IDocument document, final DocumentCommand command) {
-    String result = "";
-    try {
-      final IRegion info = document.getLineInformationOfOffset(command.offset);
-      final String text = document.get(info.getOffset(), info.getLength());
-      final Matcher matcher = Pattern.compile("^(\\s)*(\\w)").matcher(text);
-      if (matcher.find()) {
-        final int indexOf = text.indexOf(matcher.group(2));
-        result = text.substring(0, indexOf);
-      }
-    } catch (final BadLocationException e) {
-      result = "";
-    }
-    return result;
-  }
 }
